bbdf8736996159ebbdfa2440a69ea7ad3b8f3347
SimoneSalvi <simonebonemer@yahoo.com.br>
2023-03-07 23:12:30 -0300

    Add project files.

---
+++ ExercicioString2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Channels;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Informe uma string: ");
+        string texto1 = Console.ReadLine();
+
+        Console.WriteLine("Informe uma string: ");
+        string texto2 = Console.ReadLine();
+
+        // comparar duas strings
+
+        ComparaStrings(texto1, texto2);
+
+        void ComparaStrings (string a, string b)
+        {
+            if (a.Equals(b))
+            {
+                Console.WriteLine("São iguais.");
+            }
+            else
+            {
+                Console.WriteLine("São diferentes.");
+            }
+        }
+
+    }
+}
